bfb8e36240004cd751af8d7fe329902f12fe5169
Fedor Tikunov <fedor.tikunov@gmail.com>
2023-02-10 11:35:15 +0700

    lol

--- Helloc.c
+++ Helloc.c
@@ -5,9 +5,9 @@
 #define N 100000000
 
 void FuncArray(double** my_array, int len) {
+	double* temp = (double*)malloc(sizeof(double) * len);
 #pragma acc kernels
 	{
-	double* temp = (double*)malloc(sizeof(double) * len);
 	for (int i = 0; i < len; i++)
 		temp[i] = sin(i);
 	*my_array = temp;
@@ -17,7 +17,7 @@ void FuncArray(double** my_array, int len) {
 
 
 int main() {
-	double** array = (double**)malloc(sizeof(double**));
+	double** array = (double**)malloc(sizeof(double*));
 	long long len = N;
 	FuncArray(array, len);
 	return 0;
