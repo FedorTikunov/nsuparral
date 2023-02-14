d40d3ef19ce4666a433d10efbea87746b2fd3de4
Fedor Tikunov <fedor.tikunov@gmail.com>
2023-02-14 15:18:13 +0700

    lol3

--- Helloc.c
+++ Helloc.c
@@ -4,12 +4,12 @@
 #include <math.h>
 #include <time.h>
 #define N 100000000
-
+tempV = 2*3.14159265358979323846/N
 void FuncArray(double** my_array, int len) {
 	double* temp = (double*)malloc(sizeof(double) * len);
 #pragma acc kernels
 	for (int i = 0; i < len; i++)
-		temp[i] = sin(i);
+		temp[i] = sin(i*tempV);
 	*my_array = temp;
 	
 }
@@ -30,10 +30,10 @@ int main() {
 #pragma acc data create(array[:N])
 	{
 		FuncArray(array, len);
-		SumArray(array, len);
+		printf("summa = %.10f"SumArray(array, len));
 	}
 	clock_t difference = clock() - before;
-	int msec = difference * 1000 / CLOCKS_PER_SEC;
-	printf("Time taken: %d", msec/1000);
+	double msec = difference * 1000.0 / CLOCKS_PER_SEC;
+	printf("Time taken: %.5f", msec/1000);
 	return 0;
 }
