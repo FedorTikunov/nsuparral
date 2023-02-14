051c43a3580d14a9d984fe672b16c72b35130142
Fedor Tikunov <fedor.tikunov@gmail.com>
2023-02-14 17:21:55 +0700

    lol3

--- Helloc.c
+++ Helloc.c
@@ -4,21 +4,21 @@
 #include <math.h>
 #include <time.h>
 #define N 100000000
-void FuncArray(double* my_array, int len) {
+void FuncArray(double* my_array, size_t len) {
 	//double* temp = (double*)malloc(sizeof(double) * len);
 	double step = 2 * 3.14159265358979323846 / N;
 	#pragma acc data copyin(step)
 	#pragma acc parallel loop vector vector_length(128) gang
-	for (int i = 0; i < len; i++) {
+	for (size_t i = 0; i < len; i++) {
 		my_array[i] = sin(i * step);
 	}
 }
 
-double SumArray(double* my_array, int len) {
-	double sum = 0;
+double SumArray(double* my_array, size_t len) {
+	double sum = 0.0;
 	#pragma acc data copy(sum)
 	#pragma acc parallel loop reduction(+:sum) present(my_array)
-	for (int i = 0; i < len; i++) {
+	for (size_t  i = 0; i < len; i++) {
 		sum += my_array[i];
 	}
 	return sum;
@@ -26,15 +26,17 @@ double SumArray(double* my_array, int len) {
 
 int main() {
 	double* array = (double*)malloc(sizeof(double)*N);
-	long long len = N;
-	#pragma acc data create(array[0:N]) copyin(len)
+	#pragma acc data create(array[0:N])
 	{
-		clock_t before = clock();
-		FuncArray(array, len);
-		printf("summa = %0.23f\n", SumArray(array, len));
-		double sec = (double)(clock() - before)/ CLOCKS_PER_SEC;
-		printf("Time taken: %.5f", sec);
-		free(array);
+	clock_t before = clock();
+
+	FuncArray(array, N);
+	printf("summa = %0.23f\n", SumArray(array, N));
+
+	double sec = (double)(clock() - before)/ CLOCKS_PER_SEC;
+	printf("Time taken: %.5f", sec);
+
+	free(array);
 	}
 	return 0;
 }
