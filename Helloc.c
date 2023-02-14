c2ac26789d06e721a7e6bea58d728904c8505f2e
Fedor Tikunov <fedor.tikunov@gmail.com>
2023-02-14 16:03:25 +0700

    lol3

--- Helloc.c
+++ Helloc.c
@@ -16,23 +16,24 @@ void FuncArray(double* my_array, int len) {
 double SumArray(double* my_array, int len) {
 	double sum = 0;
 	#pragma acc data copy(sum)
-	#pragma acc parallel loop reduction(+:sum)
+	#pragma acc parallel loop reduction(+:sum) present(my_array)
 	for (int i = 0; i < len; i++)
 		sum += my_array[i];
 	return sum;
 }
 
 int main() {
-	clock_t before = clock();
 	double* array = (double*)malloc(sizeof(double)*N);
 	long long len = N;
 	#pragma acc data create(array[0:N]) copyin(len)
 	{
+		clock_t before = clock();
 		FuncArray(array, len);
 		printf("summa = %.10f", SumArray(array, len));
+		clock_t difference = clock() - before;
+		double msec = difference * 1000.0 / CLOCKS_PER_SEC;
+		printf("Time taken: %.5f", msec / 1000);
+		free(array);
 	}
-	clock_t difference = clock() - before;
-	double msec = difference * 1000.0 / CLOCKS_PER_SEC;
-	printf("Time taken: %.5f", msec/1000);
 	return 0;
 }
