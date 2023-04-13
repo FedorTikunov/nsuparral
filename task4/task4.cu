657e5254d0f17f3c785382c555e8e71b90ec5908
Fedor Tikunov <fedor.tikunov@gmail.com>
2023-04-13 16:22:30 +0700

    add comments

--- task4/task4.cu
+++ task4/task4.cu
@@ -9,13 +9,21 @@
 #include <cuda_runtime.h>
 #include <cub/cub.cuh>
 
+
+
+//значения в углах сетки
 #define CORN1 10.0
 #define CORN2 20.0
 #define CORN3 30.0
 #define CORN4 20.0
 
 
-
+//функция по подсчету/обновлению ячейк сетке
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//функция получает указатели двух массив. 
+//обновляет ячейки первого массива на основе среднего значения четерех ближайших по индексу ячейк из второго массива
+//функция являеться global и распоточивает подсчет матрицы на потоки
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 __global__ void calculationMatrix(double* new_arry, const double* old_array)
 {
     /*
@@ -42,9 +50,14 @@ __global__ void calculationMatrix(double* new_arry, const double* old_array)
             old_array[(i + 1) * size + j] + old_array[i * size + j + 1]);
     }
 }
-
+//функция по вычислению разницы матриц
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//функция получает указатели трех массив. 
+//модуль разницы двух первых массивов записывает в третий
+//при распоточивание, 1d массивы разбеваются на блоки по 32x32 как 2d массивы
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 __global__ void getDifferenceMatrix(const double* new_arry, const double* old_array, double* dif)
-{
+{   
     int blockIndex = blockIdx.x + gridDim.y * blockIdx.y;
     int threadIndex = threadIdx.x + threadIdx.y * blockDim.x;
 
@@ -60,25 +73,37 @@ __global__ void getDifferenceMatrix(const double* new_arry, const double* old_ar
     }
 }
 
-
+//основной код
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//получает из коммандной строки значения для размерность сетки, точности обновления сетки, максимального количества итераций
+//выделяет память на host и device для сеток
+//заполняем сетки начальными значениями
+//производим вычисления на GPU
+//выводим скорость вычисления, кол. итераций и точнось в консоль
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char** argv) {
-    int GRID_SIZE = std::stoi(argv[2]);
-    double ACC = std::pow(10, -(std::stoi(argv[1])));
-    int ITER = std::stoi(argv[3]);
-    double* newa = new double[GRID_SIZE * GRID_SIZE];
+
+    // Получаем значения из коммандной строки
+    int GRID_SIZE = std::stoi(argv[2]); // размерность сетки
+    double ACC = std::pow(10, -(std::stoi(argv[1]))); // до какой точность обновлять сетку
+    int ITER = std::stoi(argv[3]); //  максимальное количество итераций
+
+    //выделяем память под 2 сетки размера GRID_SIZExGRID_SIZE
+    double* newa = new double[GRID_SIZE * GRID_SIZE]; 
     double* olda = new double[GRID_SIZE * GRID_SIZE];
 
 
     std::memset(olda, 0, GRID_SIZE * GRID_SIZE * sizeof(double));
 
-    int iter_count = 0;
-    double error = 1.0;
-
+    int iter_count = 0; // счетчик итераций
+    double error = 1.0; // переменная ошибки
+    
     double prop1 = (CORN2 - CORN1) / (GRID_SIZE);
     double prop2 = (CORN3 - CORN1) / (GRID_SIZE);
     double prop3 = (CORN4 - CORN3) / (GRID_SIZE);
     double prop4 = (CORN2 - CORN4) / (GRID_SIZE);
 
+    //записываем значения в углы сеток
     olda[0] = CORN1;
     olda[(GRID_SIZE - 1) * GRID_SIZE] = CORN3;
     olda[GRID_SIZE - 1] = CORN2;
@@ -88,12 +113,13 @@ int main(int argc, char** argv) {
     newa[GRID_SIZE - 1] = CORN2;
     newa[GRID_SIZE - 1 + GRID_SIZE * (GRID_SIZE - 1)] = CORN4;
 
+    //выделяем память на gpu через cuda для 3 сеток
     double* d_newa,* d_olda, *d_dif;
     cudaMalloc((void**)&d_olda, sizeof(double) * GRID_SIZE * GRID_SIZE);
     cudaMalloc((void**)&d_newa, sizeof(double) * GRID_SIZE * GRID_SIZE);
     cudaMalloc((void**)&d_dif, sizeof(double) * GRID_SIZE * GRID_SIZE);
 
-
+    //вычисления значений границ сетки
     clock_t beforeinit = clock();
     for (size_t i = 1; i < GRID_SIZE - 1; i++) {
         olda[i] = olda[0] + prop1 * i;
@@ -105,54 +131,56 @@ int main(int argc, char** argv) {
         newa[(GRID_SIZE - 1) * GRID_SIZE + i] = olda[(GRID_SIZE - 1) * GRID_SIZE + i];
         newa[GRID_SIZE * i + GRID_SIZE - 1] = olda[GRID_SIZE * i + GRID_SIZE - 1];
     }
-
+    
+    // размерность блоков и грида 
     dim3 block_dim(32, 32);
     dim3 grid_dim(GRID_SIZE / block_dim.x, GRID_SIZE/ block_dim.y);
 
-    cudaMemcpy(d_olda, olda, sizeof(double) * GRID_SIZE * GRID_SIZE, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_newa, newa, sizeof(double) * GRID_SIZE * GRID_SIZE, cudaMemcpyHostToDevice);
+    // копирование информации с CPU на GPU
+    cudaMemcpy(d_olda, olda, sizeof(double) * GRID_SIZE * GRID_SIZE, cudaMemcpyHostToDevice); // (CPU) olda -> (GPU) d_olda
+    cudaMemcpy(d_newa, newa, sizeof(double) * GRID_SIZE * GRID_SIZE, cudaMemcpyHostToDevice); // (CPU) newa -> (GPU) d_newa
+
+    //выделяем память на gpu для переменной, которая будет хранить ошибку на device
     double* max_error = 0;
     cudaMalloc((void**)&max_error, sizeof(double));
+
     std::cout << "Initialization time: " << 1.0 * (clock() - beforeinit) / CLOCKS_PER_SEC << std::endl;
+
     size_t temp_storage_bytes = 0;
     double* temp_storage = NULL;
+    //получаем размер временного буфера для редукции
     cub::DeviceReduce::Max(temp_storage, temp_storage_bytes, d_dif, max_error, GRID_SIZE * GRID_SIZE);
+
+    //выделяем память для буфера
     cudaMalloc((void**)&temp_storage, temp_storage_bytes);
 
     clock_t beforecal = clock();
-    double beta = -1.0;
+    
+    //алгоритм обновления сетки, работающий пока макс. ошибка не станет меньше или равне нужной точности, или пока количество итерации не превысит максимальное количество.
     while (iter_count < ITER && error > ACC) {
         iter_count++;
-        calculationMatrix <<<GRID_SIZE-1, GRID_SIZE-1>>> (d_newa, d_olda);
+        calculationMatrix <<<GRID_SIZE-1, GRID_SIZE-1>>> (d_newa, d_olda); // расчет матрицы
 
+        // рфсчитываем ошибку каждую сотую итерацию
         if(iter_count % 100 == 0){
-            getDifferenceMatrix <<<grid_dim, block_dim >>> (d_newa, d_olda, d_dif);
-            cub::DeviceReduce::Max(temp_storage, temp_storage_bytes, d_dif, max_error, GRID_SIZE * GRID_SIZE);
-            cudaMemcpy(&error, max_error, sizeof(double), cudaMemcpyDeviceToHost);
+            getDifferenceMatrix <<<grid_dim, block_dim >>> (d_newa, d_olda, d_dif); // вычисления разницы матрицы
+            cub::DeviceReduce::Max(temp_storage, temp_storage_bytes, d_dif, max_error, GRID_SIZE * GRID_SIZE); // нахождение максимума в разнице матрицы
+            cudaMemcpy(&error, max_error, sizeof(double), cudaMemcpyDeviceToHost); // запись ошибки в переменную на host
             error = std::abs(error);
         }
 
+        //смена указателей между сетками на device
         double* c = d_olda;
         d_olda = d_newa;
         d_newa = c;
     }
     
-    /*
-    cudaMemcpy((void*)olda, (void*)d_olda, sizeof(double) * GRID_SIZE * GRID_SIZE, cudaMemcpyDeviceToHost);
-
-    for (size_t i = 0; i < GRID_SIZE; i++)
-    {
-        for (size_t j = 0; j < GRID_SIZE; j++)
-        {
-            std::cout << olda[i * GRID_SIZE + j] << " ";
-        }
-        std::cout << std::endl;
-    }
-    */
-
+    //вывод времени работы на алгоритма
     std::cout << "Calculation time: " << 1.0 * (clock() - beforecal) / CLOCKS_PER_SEC << std::endl;
+    //вывод кол. итерацций и значение ошибки
     std::cout << "Iteration: " << iter_count << " " << "Error: " << error << std::endl;
 
+    //очитска памяти
     cudaFree(d_olda);
     cudaFree(d_newa);
     cudaFree(temp_storage);
