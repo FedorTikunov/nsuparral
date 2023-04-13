b179f93a8af7597b802a700634f795285e55365e
Fedor Tikunov <fedor.tikunov@gmail.com>
2023-04-13 16:33:09 +0700

    add comments

--- task4/task4.cu
+++ task4/task4.cu
@@ -43,8 +43,8 @@ __global__ void calculationMatrix(double* new_arry, const double* old_array)
     */
     size_t i = blockIdx.x;
     size_t j = threadIdx.x;
-    size_t size = blockDim.x;
-    printf("%d", size);
+    size_t size = blockDim.x + 1;
+    //printf("%d", size);
     if (i != 0 && i != size - 1 && j != 0 && j != size - 1)
     {
         new_arry[i * size + j] = 0.25 * (old_array[i * size + j - 1] + old_array[(i - 1) * size + j] +
