33233b1f44d3c0eafc6e9375f70fcec5e876f486
Fedor Tikunov <fedor.tikunov@gmail.com>
2023-04-13 16:28:11 +0700

    add comments

--- task4/task4.cu
+++ task4/task4.cu
@@ -44,7 +44,7 @@ __global__ void calculationMatrix(double* new_arry, const double* old_array)
     size_t i = blockIdx.x;
     size_t j = threadIdx.x;
     size_t size = blockDim.x
-    if (i != 0 && i != GRID_SIZEY - 1 && j != 0 && j != GRID_SIZEX - 1)
+    if (i != 0 && i != blockDim.y - 1 && j != 0 && j != size - 1)
     {
         new_arry[i * size + j] = 0.25 * (old_array[i * size + j - 1] + old_array[(i - 1) * size + j] +
             old_array[(i + 1) * size + j] + old_array[i * size + j + 1]);
